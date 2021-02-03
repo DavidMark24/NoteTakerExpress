3b15e38809f29ee62030a8c8013ddc3d6622510a
David Johnson <david.m.johnson32@gmail.com>
2021-02-02 18:46:10 -0800

    Added get request

---
+++ server.js
@@ -0,0 +1,16 @@
+// Required npm packages 
+
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+const app = express();
+
+// PORT 
+
+const PORT = process.env.PORT || 3000;
+
+// Data paarsing for express app
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
